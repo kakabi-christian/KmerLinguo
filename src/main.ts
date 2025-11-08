ea2a393950bc8014504631c1f7faa0d930b6726e
Kakabi Christian <kakabichristian@gmail.com>
2025-11-08 23:34:52 +0100

    mise a jour du main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:51506'], // ton app Flutter Web
+    origin: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
